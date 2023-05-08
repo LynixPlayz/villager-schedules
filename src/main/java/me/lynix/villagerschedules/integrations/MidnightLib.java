efec35998b44b4cf84a5cedc0f186285da6a56fe
LynixPlayz <82721356+LynixPlayz@users.noreply.github.com>
2023-05-07 20:00:19 -0500

    Added category names and slightly modified the base Config values to look more natural.

--- src/main/java/me/lynix/villagerschedules/integrations/MidnightLib.java
+++ src/main/java/me/lynix/villagerschedules/integrations/MidnightLib.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class MidnightLib extends MidnightConfig {
     @Entry(category = "enabled", name = "Enabled") public static boolean enabled = true;
     @Entry(category = "position", name = "x") public static int x = 2;
-    @Entry(category = "position", name = "y") public static int y = 0;
+    @Entry(category = "position", name = "y") public static int y = 2;
 }
